22da2edec4af898d631ad4c57215cf99af7e0ebf
hare1039 <hare39393@gmail.com>
2017-06-16 16:17:07 +0800

    Remove bad style of C++ code in direct_mapped_cache.cpp

--- C++_code/direct_mapped_cache.cpp
+++ C++_code/direct_mapped_cache.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <stdio.h>
-#include <math.h>
+#include <cstdio>
+#include <cmath>
 
-using namespace std;
-
-struct cache_content{
-	bool v;
-	unsigned int  tag;
-//	unsigned int	data[16];    
+struct cache_content
+{
+    bool v;
+    unsigned int tag;
+//  unsigned int data[16];    
 };
 
 const int K=1024;
@@ -19,39 +18,44 @@ double log2( double n )
 }
 
 
-void simulate(int cache_size, int block_size){
-	unsigned int tag,index,x;
-
-	int offset_bit = (int) log2(block_size);
-	int index_bit = (int) log2(cache_size/block_size);
-	int line= cache_size>>(offset_bit);
-
-	cache_content *cache =new cache_content[line];
-	cout<<"cache line:"<<line<<endl;
-
-	for(int j=0;j<line;j++)
-		cache[j].v=false;
-	
-  FILE * fp=fopen("test.txt","r");					//read file
-	
-	while(fscanf(fp,"%x",&x)!=EOF){
-		cout<<hex<<x<<" ";
-		index=(x>>offset_bit)&(line-1);
-		tag=x>>(index_bit+offset_bit);
-		if(cache[index].v && cache[index].tag==tag){
-			cache[index].v=true; 			//hit
-		}
-		else{						
-			cache[index].v=true;			//miss
-			cache[index].tag=tag;
-		}
-	}
-	fclose(fp);
-
-	delete [] cache;
+void simulate(int cache_size, int block_size)
+{
+    unsigned int tag, index, x;
+    
+    int offset_bit = static_cast<int>(log2(block_size));
+    int index_bit  = static_cast<int>(log2(cache_size/block_size));
+    int line = cache_size >> (offset_bit);
+    
+    cache_content *cache = new cache_content[line];
+    std::cout << "cache line:" << line << std::endl;
+    
+    for(int j = 0; j < line; j++)
+    	cache[j].v = false;
+    
+    FILE * fp = fopen("test.txt","r");					//read file using c styled
+    
+    while(fscanf(fp,"%x",&x)!=EOF)
+    {
+    	std::cout << std::hex << x << " ";
+    	index = (x >> offset_bit) & (line-1);
+    	tag   =  x >> (index_bit + offset_bit);
+    	if(cache[index].v && cache[index].tag==tag)
+	{
+	    cache[index].v = true; 			//hit
+    	}
+    	else
+	{						
+	    cache[index].v   = true;			//miss
+	    cache[index].tag = tag;
+    	}
+    }
+    fclose(fp);
+    
+    delete [] cache;
 }
 	
-int main(){
+int main()
+{
 	// Let us simulate 4KB cache with 16B blocks
-	simulate(4*K, 16);
+    simulate(4*K, 16);
 }

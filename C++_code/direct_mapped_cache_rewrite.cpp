05464b6fcd2f60ee117d2e6005905cc617d0a877
hare1039 <hare39393@gmail.com>
2017-06-20 20:35:08 +0800

    Make direct_mapped_cache_rewrite.cpp into more moden style

--- C++_code/direct_mapped_cache_rewrite.cpp
+++ C++_code/direct_mapped_cache_rewrite.cpp
@@ -1,76 +1,63 @@
 #include <iostream>
-#include <cstdio>
+#include <fstream>
 #include <cmath>
 
 struct cache_content
 {
-    bool         v;
+    bool         v = false;
     unsigned int tag;
     int          last_time;
 };
+typedef unsigned long long int ulli;
+ulli operator"" _KB (ulli x) { return x * 1024; }
+ulli operator"" _B  (ulli x) { return x; }
+ulli operator"" _way(ulli x) { return x; }
 
-const int K=1024;
 
-double log2( double n )
+void simulate(int way, int cache_size, int block_size, std::string &&file_name)
 {
-    // log(n)/log(2) is log2.
-    return log( n ) / log(double(2));
-}
-
-
-void simulate(int associativity, int cache_size, int block_size, const char *file_name)
-{
-    //miss rate
-    int number_lookup = 0;
-    int number_miss   = 0;
-
-    unsigned int tag,index,x;
-
+    std::function<double(double)> log2 = [](double n){return std::log(n) / std::log(2);};
     int offset_bit = static_cast<int>( log2(block_size) );
-    int index_bit  = static_cast<int>( log2(cache_size/block_size/associativity) );
+    int index_bit  = static_cast<int>( log2(cache_size/block_size/way) );
     int line       = 1 << index_bit;
 
     cache_content **cache = new cache_content*[line];
-    for(int i=0;i<line;++i)
+    for(int i(0) ; i < line ; i++)
     {
-        cache[i] = new cache_content[associativity];
+        cache[i] = new cache_content[way];
     }
 
-    for(int j=0;j<line;j++)
-        for(int k=0;k<associativity;++k)
-            cache[j][k].v=false;
-
-    FILE *fp=fopen(file_name,"r");					//read file
-
-    while(fscanf(fp,"%x",&x)!=EOF)
+    std::ifstream input(file_name);
+    ulli addr, time_x(0), miss_time(0);
+    while(input >> std::hex >> addr)
     {
-	index=(x>>offset_bit)&(line-1);
-	tag=x>>(index_bit+offset_bit);
-
+	unsigned int tag, index;
+	index = (addr >> offset_bit) & (line - 1);
+        tag   =  addr >> (index_bit + offset_bit);
         bool hit = false;
-        for(int i=0;i<associativity;++i)
+        for(int i(0); i < way; i++)
 	{
             if( cache[index][i].v && cache[index][i].tag == tag )
 	    { // hit
                 hit = true;
-                cache[index][i].last_time = number_lookup;
+                cache[index][i].last_time = time_x;
                 break;
             }
         }
 
-        if(!hit)
-	{ // miss
-            number_miss++;
+        if(not hit)
+	{ 
+            miss_time++;
 
             int find_empty = false;
-            for(int i=0;i<associativity;++i)
+            for(int i(0); i < way; i++)
 	    {
                 if( cache[index][i].v == false )
 		{
                     find_empty = true;
                     cache[index][i].v         = true;
                     cache[index][i].tag       = tag;
-                    cache[index][i].last_time = number_lookup;
+                    cache[index][i].last_time = time_x;
                     break;
                 }
             }
@@ -79,7 +66,7 @@ void simulate(int associativity, int cache_size, int block_size, const char *fil
                 int min_time  = 99999;
                 int index_min = -1;
 
-                for(int i=0;i<associativity;++i)
+                for(int i(0); i < way; i++)
 		{
                     if(min_time > cache[index][i].last_time)
 		    {
@@ -89,46 +76,42 @@ void simulate(int associativity, int cache_size, int block_size, const char *fil
                 }
                 cache[index][index_min].v         = true;
                 cache[index][index_min].tag       = tag;
-                cache[index][index_min].last_time = number_lookup;
+                cache[index][index_min].last_time = time_x;
             }
 
         }
-	number_lookup++;
+	time_x++;
     }
-    fclose(fp);
 
     std::cout << std::endl;
     std::cout << "file_name:\t"     << file_name     << std::endl;
-    std::cout << "associativity:\t" << associativity << std::endl;
+    std::cout << "way:   \t"        << way           << std::endl;
     std::cout << "cache_size:\t"    << cache_size    << std::endl;
     std::cout << "block_size:\t"    << block_size    << std::endl;
-    std::cout << "number_lookup:\t" << number_lookup << std::endl;
-    std::cout << "number_miss:\t"   << number_miss   << std::endl;
-    std::cout << "miss rate:\t"     << static_cast<double>(number_miss)/number_lookup << std::endl;
+    std::cout << "time_x:\t"        << time_x        << std::endl;
+    std::cout << "miss_time:\t"     << miss_time     << std::endl;
+    std::cout << "miss rate:\t"     << static_cast<double>(miss_time)/time_x << std::endl;
     std::cout << std::endl;
 
-    for(int i=0;i<line;++i)
+    for(int i(0); i < line; i++)
     {
         delete [] cache[i];
     }
     delete [] cache;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    char file_name[2][11]={"LU.txt", "RADIX.txt"};
-	// Let us simulate 4KB cache with 16B blocks
-    int block_size = 64;
-    for(int i=0;i<2;++i)
+    int block_size = 64_B;
+    
+    for(int ways(1_way); ways < 16; ways <<= 1)
     {
-        for(int associativity=1;associativity < 16;associativity*=2)
+	for(int cache_size(1_KB); cache_size < 64_KB; cache_size <<= 1)
 	{
-            for(int cache_size=1*K;cache_size<64*K;cache_size*=2)
-	    {
-                simulate(associativity, cache_size, block_size, file_name[i]);
-            }
-        }
+	    simulate(ways, cache_size, block_size, std::string(argv[1]));
+	}
     }
+    
 
     return 0;
 }
